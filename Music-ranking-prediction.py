45c70f61f5c42e8338303520bf17064aa64b8d5a
jjony553 <55618626+jjony553@users.noreply.github.com>
2019-09-23 00:36:48 +0900

    코드수정
    변경사항
    1. 등록번호 자릿수 오류 수정
    해야할 것
    1. 5주는 출력이 안되고 있음
    2. 10월부터는 str(month)앞 0을 수정해야함 ex) 09에서 10으로 가면 010으로 됨

--- Music-ranking-prediction.py
+++ Music-ranking-prediction.py
@@ -1,7 +1,7 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
-year = 2013
+year = 2012
 month = 1
 week = 1
 
@@ -14,11 +14,10 @@ while (1):
         month = 1
         year += 1
 
-    if year is 2019 and month is 9 and week is 4:
+    if year is 2019 and month is 9 and week is 3:
         print("end")
         break
-
-
+#현재 사이트에 2주까지 있어서 week은 3로 설정
    
     myurl = "https://music.naver.com/listen/history/index.nhn?type=TOTAL&year="+str(year)+"&month=0"+str(month)+"&week="+str(week)
     print(myurl)
@@ -26,21 +25,27 @@ while (1):
 
     soup = BeautifulSoup(url,"lxml")
     temp = []
-    for link1 in soup.find_all(name="td",attrs={"class":"ico"}):
+    for link1 in soup.find_all(name="td",attrs={"class":"name"}):
+        
         try:
-            a = link1.select('a')[0]['href'] #a 태그에서 href만 가져오기
+            a = link1.select('a')[3]['href'] #3번 a 태그에서 href만 가져오기
             a = a.strip("#")
+
             if a is "":
                 pass
             else:
-                temp.append(a)      
+                temp.append(a)
         except:
             pass
             #print("href 없음")
+        
 
     week += 1
     print(temp)
+    
 
 
-print(temp)
+
+
+
 
