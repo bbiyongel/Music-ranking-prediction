eb7e9f242a492227280af38de8db17a3473746c9
jinho <jinho021712@gmail.com>
2019-09-22 16:01:23 +0900

    코드수정
    while문 추가
    year, month, week값 증가,
    배열에 값 얻어오는거 확인

--- Music-ranking-prediction.py
+++ Music-ranking-prediction.py
@@ -1,30 +1,46 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
-year = 2019
-month = 9
-week = 2
+year = 2013
+month = 1
+week = 1
 
- 
-url = urlopen("https://music.naver.com/listen/history/index.nhn?type=TOTAL&year=%d&month=0%d&week=%d" %(year,month,week))
-#url = urlopen("https://music.naver.com/listen/history/index.nhn?type=TOTAL&year=2019&month=09&week=02") 위와 URL주소 동일
-#현재 주소가 차트 히스토리 주소, 주별로 뽑기위해 위 링크로 설정
+while (1):
+    if week is 5:
+        week = 1
+        month += 1
 
-soup = BeautifulSoup(url,"lxml")
-temp = []
-for link1 in soup.find_all(name="td",attrs={"class":"ico"}):
-    try:
-        a = link1.select('a')[0]['href'] #a 태그에서 href만 가져오기
-        a = a.strip("#")
-        if a is "":
+    if month is 13:
+        month = 1
+        year += 1
+
+    if year is 2019 and month is 9 and week is 4:
+        print("end")
+        break
+
+
+   
+    myurl = "https://music.naver.com/listen/history/index.nhn?type=TOTAL&year="+str(year)+"&month=0"+str(month)+"&week="+str(week)
+    print(myurl)
+    url = urlopen(myurl)
+
+    soup = BeautifulSoup(url,"lxml")
+    temp = []
+    for link1 in soup.find_all(name="td",attrs={"class":"ico"}):
+        try:
+            a = link1.select('a')[0]['href'] #a 태그에서 href만 가져오기
+            a = a.strip("#")
+            if a is "":
+                pass
+            else:
+                temp.append(a)      
+        except:
             pass
-        else:
-            temp.append(a)      
-    except:
-        pass
-        #print("href 없음")
+            #print("href 없음")
+
+    week += 1
+    print(temp)
+
+
 print(temp)
-  
- 
-# 현재 2012년부터 2017년은 번호 자릿수가 두 칸 잘려나오고 50위까지 전부 나오지 않음
-# 번호가 틀렸던 우리 둘 URL이 잘못 됐었음. 난 차트 히스토리 URL 넌 Top 100차트 URL 이것 때문에 번호가 다르게 나옴
+
