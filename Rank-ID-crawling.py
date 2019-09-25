b950ba02f3c99e9e72ff9d5dffe873d8fd0d0b2f
JinhoLee <jinho021712@gmail.com>
2019-09-26 00:59:32 +0900

    csv 형식으로 받아오기 성공

--- Rank-ID-crawling.py
+++ Rank-ID-crawling.py
@@ -3,27 +3,39 @@ from bs4 import BeautifulSoup
 import pandas as pd
 from pandas import DataFrame
 
-year = 2019
-month = 8
+#수집기간 2012년 1월 1째 주 부터 2019년 9월 까지
+year = 2012
+month = 1
 week = 1
-temp = []
-ranks = []
+#temp = []
+#ranks = []
 
 
 
 while (1):
+    temp = []
+    ranks = []
+    if year is 2019:
+        #if문 무시하고 계속해서 숫자가 늘어나는 문제가 있음 
+        if month is 10:
+            #if week is 3:
+            print("end")
+            break
+    else:
+        pass
     if week is 5:
         week = 1
         month += 1
-
+    else:
+        pass
     if month is 13: 
         month = 1
         year += 1
+    else:
+        pass
 
-    if year is 2019 and month is 9 and week is 3:
-        print("end")
-        break
-#현재 사이트에 2주까지 있어서 week은 3로 설정
+    
+    #현재 사이트에 2주까지 있어서 week은 3로 설정
     remonth = str(month).rjust(2, '0')
     myurl = "https://music.naver.com/listen/history/index.nhn?type=TOTAL&year="+str(year)+"&month="+str(remonth)+"&week="+str(week)
     print(myurl)
@@ -40,28 +52,24 @@ while (1):
             if a is "":
                 pass
             else:
-                temp.append(a)
-                
-                
-                
+                temp.append(a)         
         except:
             pass
             #print("href 없음")
         
     for rank in soup.find_all(name="td",attrs={"class":"ranking"}):
-        
         try:  
             span = rank.text
             ranks.append(span)
-            
+
         except:
             pass
-            #print("href 없음")
-    
+            #print("href 없음")   
     week += 1
     
-data1 = DataFrame(ranks,columns=['rank'])  
-data2 = DataFrame(temp,columns=['songInfo'])
-data = pd.concat([data1,data2], axis = 1)
-data.to_csv('idval.csv', encoding='utf-8')  
+    data1 = DataFrame(ranks)  
+    data2 = DataFrame(temp)
+    data = pd.concat([data1,data2], axis = 1)
+    data = pd.DataFrame(data)
+    data.to_csv('idval.csv', mode='a', encoding='utf-8',header=None)  
 print("저장성공")   
