093ee3dd915a8cbc47af3ee9ef39e67ec5ea2a73
JinhoLee <jinho021712@gmail.com>
2019-09-26 23:35:01 +0900

    가수, 작곡, 작사, 편곡 정보 파싱
    가수, 작곡, 작사, 편곡 순으로 파싱 성공
    리스트에 넣고 csv파일로 저장해야한다.

--- Get-dataSet.py
+++ Get-dataSet.py
@@ -1,55 +1,62 @@
+import csv
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import pandas as pd
 from pandas import DataFrame
 
-df = pd.read_csv('./idval.csv',sep=',')
 
-column_df = df['songNum']
+f = open('idval.csv', 'r')
+reader = csv.reader(f)
 
-
-songInfo = []
-
-
-
-#myurl = "https://music.naver.com/lyric/index.nhn?trackId="+str(column_df)
-
-myurl = "https://music.naver.com/lyric/index.nhn?trackId=28450269"
-print(myurl)
-url = urlopen(myurl)
-
-soup = BeautifulSoup(url,"lxml")
-
-for info in soup.find_all(name="span",attrs={"class":"info"}):
-    
+for row in reader:
+    rank = row[0] #rank
+    URL_ID = row[1] #URL Num ID
+    myurl = "https://music.naver.com/lyric/index.nhn?trackId="+str(URL_ID)
+    #print(myurl)
+    url = urlopen(myurl)
+    soup = BeautifulSoup(url,"lxml")
     try:
-        
-        info = info.get_text()
-        
-        songInfo.append(info)
-        
-        '''
-        if info is "\n":
-            pass
-        elif info is "\t":
-            pass
-        else:
-            songInfo.append(info)
-        '''
-        #\t,\n,\r이 같이 출력됨
-               
-            
+        get_Sing = soup.find(name="span",attrs={"class":"artist"})
+        get_Sing = get_Sing.select('a')[0]['title']
+        print(get_Sing)
     except:
+        print('가수정보없음')
         pass
-        #print("href 없음")
-   
+    try:
+        get_song_info = soup.find(name="p",attrs={"class":"song_info"})#작곡가
+
+        try:
+            get_Composer = get_song_info.select('span')[0]
+            get_Composer = get_Composer.select('a')[0]
+            get_Composer = get_Composer.text
+            print(get_Composer.strip()) #공백제거
+        except:
+            print('작곡가정보없음')
+            pass
+        try:
+            get_Lyricist = get_song_info.select('span')[1]
+            get_Lyricist = get_Lyricist.select('a')[0]
+            get_Lyricist = get_Lyricist.text
+            print(get_Lyricist.strip()) #공백제거
+        except:
+            print('작사가정보없음')
+            pass
+        try:
+            get_Arrangement = get_song_info.select('span')[2]
+            get_Arrangement = get_Arrangement.select('a')[0]
+            get_Arrangement = get_Arrangement.text
+            print(get_Arrangement.strip()) #공백제거
+        except:
+            print('편곡정보없음')
+            pass
+    except:
+        print('song info NULL')
+        pass
+#csv 파일 이름은 dataset.csv 로 한다.
+
+f.close()
 
 
 
-print(songInfo)
 
-#data1 = DataFrame(ranks,columns=['rank'])  
-#data2 = DataFrame(temp,columns=['songInfo'])
-#data = pd.concat([data1,data2], axis = 1)
-#data.to_csv('idval.csv', encoding='utf-8')  
-print("저장성공")   
+
