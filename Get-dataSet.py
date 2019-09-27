cbdc7ae0c173d01a71d1f7d806f9a40e62d1c7e3
jjony553 <55618626+jjony553@users.noreply.github.com>
2019-09-27 18:18:02 +0900

    append 추가
    파일 csv로 저장시 한글깨짐

--- Get-dataSet.py
+++ Get-dataSet.py
@@ -8,6 +8,11 @@ from pandas import DataFrame
 f = open('idval.csv', 'r')
 reader = csv.reader(f)
 
+data1 = []
+data2 = []
+data3 = []
+data4 = []
+
 for row in reader:
     rank = row[0] #rank
     URL_ID = row[1] #URL Num ID
@@ -18,6 +23,7 @@ for row in reader:
     try:
         get_Sing = soup.find(name="span",attrs={"class":"artist"})
         get_Sing = get_Sing.select('a')[0]['title']
+        data1.append(get_Sing)
         print(get_Sing)
     except:
         print('가수정보없음')
@@ -29,7 +35,9 @@ for row in reader:
             get_Composer = get_song_info.select('span')[0]
             get_Composer = get_Composer.select('a')[0]
             get_Composer = get_Composer.text
-            print(get_Composer.strip()) #공백제거
+            get_Composer.strip() #공백제거
+            data2.append(get_Composer)
+            print(get_Composer)
         except:
             print('작곡가정보없음')
             pass
@@ -37,7 +45,9 @@ for row in reader:
             get_Lyricist = get_song_info.select('span')[1]
             get_Lyricist = get_Lyricist.select('a')[0]
             get_Lyricist = get_Lyricist.text
-            print(get_Lyricist.strip()) #공백제거
+            get_Lyricist.strip() #공백제거
+            data3.apped(get_Lyricist)
+            print(get_Lyricist)
         except:
             print('작사가정보없음')
             pass
@@ -45,15 +55,24 @@ for row in reader:
             get_Arrangement = get_song_info.select('span')[2]
             get_Arrangement = get_Arrangement.select('a')[0]
             get_Arrangement = get_Arrangement.text
-            print(get_Arrangement.strip()) #공백제거
+            get_Arrangement.strip() #공백제거
+            data4.append(get_Arrangement)
+            print(get_Arrangement)
         except:
             print('편곡정보없음')
             pass
     except:
         print('song info NULL')
         pass
-#csv 파일 이름은 dataset.csv 로 한다.
 
+data1 = DataFrame(data1)
+data2 = DataFrame(data2)
+data3 = DataFrame(data3)
+data4 = DataFrame(data4)
+data = pd.concat([data1,data2,data3,data4], axis = 1)
+data = pd.DataFrame(data)
+data.to_csv('dataset.csv', mode='a', encoding='utf-8',header=None)
+print("저장성공")   
 f.close()
 
 
