3f22bdf04f7905499276d432307142b8e615b4b8
jjony553 <55618626+jjony553@users.noreply.github.com>
2019-09-27 22:34:14 +0900

    코드 수정
    CSV에 한글적용 완료
    -수정할 것-
    1. URL_ID를 잘 못받아 오는지 계속 볼사는 안나오고 김건모를 찾고있음, 볼사 URL로 했을시 엑셀에 잘 나옴
    2. 네이버뮤직에 작사나 작곡이 많으면 이름이 ...으로 표시되는데 이 경우 없음으로 뜸

--- Get-dataSet.py
+++ Get-dataSet.py
@@ -16,15 +16,17 @@ data4 = []
 for row in reader:
     rank = row[0] #rank
     URL_ID = row[1] #URL Num ID
-    myurl = "https://music.naver.com/lyric/index.nhn?trackId="+str(URL_ID)
+    #myurl = "https://music.naver.com/lyric/index.nhn?trackId="+str(URL_ID)
+    myurl = "https://music.naver.com/lyric/index.nhn?trackId=28450269"
     #print(myurl)
     url = urlopen(myurl)
     soup = BeautifulSoup(url,"lxml")
     try:
         get_Sing = soup.find(name="span",attrs={"class":"artist"})
         get_Sing = get_Sing.select('a')[0]['title']
+        #print(get_Sing)
         data1.append(get_Sing)
-        print(get_Sing)
+        
     except:
         print('가수정보없음')
         pass
@@ -36,8 +38,9 @@ for row in reader:
             get_Composer = get_Composer.select('a')[0]
             get_Composer = get_Composer.text
             get_Composer.strip() #공백제거
+            #print(get_Composer)
             data2.append(get_Composer)
-            print(get_Composer)
+            
         except:
             print('작곡가정보없음')
             pass
@@ -46,8 +49,9 @@ for row in reader:
             get_Lyricist = get_Lyricist.select('a')[0]
             get_Lyricist = get_Lyricist.text
             get_Lyricist.strip() #공백제거
+            #print(get_Lyricist)
             data3.apped(get_Lyricist)
-            print(get_Lyricist)
+            
         except:
             print('작사가정보없음')
             pass
@@ -56,8 +60,9 @@ for row in reader:
             get_Arrangement = get_Arrangement.select('a')[0]
             get_Arrangement = get_Arrangement.text
             get_Arrangement.strip() #공백제거
+            #print(get_Arrangement)
             data4.append(get_Arrangement)
-            print(get_Arrangement)
+               
         except:
             print('편곡정보없음')
             pass
@@ -71,7 +76,7 @@ data3 = DataFrame(data3)
 data4 = DataFrame(data4)
 data = pd.concat([data1,data2,data3,data4], axis = 1)
 data = pd.DataFrame(data)
-data.to_csv('dataset.csv', mode='a', encoding='utf-8',header=None)
+data.to_csv('dataset.csv', mode='a', encoding='euc-kr',header=None)
 print("저장성공")   
 f.close()
 
