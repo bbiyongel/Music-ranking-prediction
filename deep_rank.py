904b7134268aa6fc7057290cec1a46392c1f68a0
JinhoLee <jinho021712@gmail.com>
2019-10-08 16:13:51 +0900

    사람, 그룹에 인덱스 넘버를 붙임

--- deep_rank.py
+++ deep_rank.py
@@ -1,20 +1,42 @@
 import pandas as pd
 from collections import Counter
-
-mydataset = pd.read_csv('dataset.csv', encoding = 'EUC-KR')
+import numpy as np
+mydataset = pd.read_csv('dataset2.csv', encoding = 'EUC-KR')
 print(mydataset.shape)
 
 #빈도수를 보자
 mydatalist = []
-data1 = mydataset['작곡']
-mydatalist += list(data1)
-data2 = mydataset['작사']
-mydatalist += list(data2)
-data3 = mydataset['편곡']
-mydatalist += list(data3)
-data4 = mydataset['가수']
-mydatalist += list(data4)
-print(len(mydatalist))
+data1 = list(mydataset['작곡'])
+mydatalist += data1
+data2 = list(mydataset['작사'])
+mydatalist += data2
+data3 = list(mydataset['편곡'])
+mydatalist += data3
+data4 = list(mydataset['가수'])
+mydatalist += data4
+mydatalist = list(set(mydatalist))
+#print(mydatalist)
 
-counts = Counter(mydatalist)
-print(counts)
+new_list = []
+
+for i in range(0, len(mydatalist)):
+        
+    temp = []
+    temp.append(mydatalist[i])
+    temp.append(i)
+    new_list.append(temp)
+
+
+new_list = np.array(new_list)
+slice_col = list(new_list[:,0]) #열만 자르기
+#인덱스 저장
+#data = pd.DataFrame(new_list)
+#data.to_csv('index_num.txt', mode='a', encoding='utf-8',header=None)
+
+for data_cg in data4:
+    #문자열을 숫자로(라벨 붙이기)
+    temp2 = []
+    temp2.append(slice_col.index(str(data_cg)))#인덱스 찾기
+    #파일 이름을 인덱스 번호로 바꿔주고 저장, 새로운 dataset만들기
+    data = pd.DataFrame(temp2)
+    data.to_csv('가수.txt', mode='a', encoding='utf-8',header=None)
