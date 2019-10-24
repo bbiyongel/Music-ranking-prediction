79714760368205c252cca980f7a17e4a12dfc7ba
JinhoLee <jinho021712@gmail.com>
2019-10-25 01:43:21 +0900

    코드수정, 딥러닝 코드 추가
    아티스트 인덱스를 붙이는 과정에서
    인덱스와 아티스트명이 맞지 않는 오류 발견, 코드수정 완료
    
    딥러닝 코드 테스트 중

--- python_files/deep_rank.py
+++ python_files/deep_rank.py
@@ -4,10 +4,10 @@
 import pandas as pd
 from collections import Counter
 import numpy as np
-mydataset = pd.read_csv('dataset2.csv', encoding = 'EUC-KR')
+mydataset = pd.read_csv('../collected_data/dataset2.csv', encoding = 'EUC-KR')
 print(mydataset.shape)
 
-#빈도수를 보자
+
 mydatalist = []
 data1 = list(mydataset['작곡'])
 mydatalist += data1
@@ -18,7 +18,6 @@ mydatalist += data3
 data4 = list(mydataset['가수'])
 mydatalist += data4
 mydatalist = list(set(mydatalist))
-#print(mydatalist)
 
 new_list = []
 
@@ -26,20 +25,21 @@ for i in range(0, len(mydatalist)):
         
     temp = []
     temp.append(mydatalist[i])
-    temp.append(i)
-    new_list.append(temp)
+    new_list += temp
+
+new_list = list(map(str, new_list))
+new_list = sorted(new_list)
 
 
-new_list = np.array(new_list)
-slice_col = list(new_list[:,0]) #열만 자르기
 #인덱스 저장
 #data = pd.DataFrame(new_list)
 #data.to_csv('index_num.txt', mode='a', encoding='utf-8',header=None)
 
+
 for data_cg in data4:
     #문자열을 숫자로(라벨 붙이기)
     temp2 = []
-    temp2.append(slice_col.index(str(data_cg)))#인덱스 찾기
-    #파일 이름을 인덱스 번호로 바꿔주고 저장, 새로운 dataset만들기
+    temp2.append(new_list.index(str(data_cg)))#인덱스 찾기
+    #이름을 인덱스 번호로 바꿔주고 저장, 새로운 dataset만들기
     data = pd.DataFrame(temp2)
     data.to_csv('가수.txt', mode='a', encoding='utf-8',header=None)
---
+++ python_files/deep_test.py
@@ -0,0 +1,101 @@
+#기본 문법으로 예측해보기
+"""
+import numpy as np
+import pandas as pd
+
+df = pd.DataFrame({'name': ['아이유', '아이즈원', '폴킴', '트와이스', '지코', '존박', '트와이스'],
+                   'score': [1, 2, 3, 4, 5, 6, 7]}, columns=['name', 'score'])
+
+df['rank_by_average'] = df['score'].rank(ascending=False)
+df['rank_by_min'] = df['score'].rank(method='min', ascending=False)
+df['rank_by_max'] = df['score'].rank(method='max', ascending=False)
+df['rank_by_first'] = df['score'].rank(method='first', ascending=False)
+df['rank_by_dense'] = df['score'].rank(method='dense', ascending=False)
+
+print(df)
+
+'''
+#output (점수가 기준이라 순위가 뒤바뀜)
+   name  score  rank_by_average  ...  rank_by_max  rank_by_first  rank_by_dense
+0   아이유      1              7.0  ...          7.0            7.0            7.0
+1  아이즈원      2              6.0  ...          6.0            6.0            6.0
+2    폴킴      3              5.0  ...          5.0            5.0            5.0
+3  트와이스      4              4.0  ...          4.0            4.0            4.0
+4    지코      5              3.0  ...          3.0            3.0            3.0
+5    존박      6              2.0  ...          2.0            2.0            2.0
+6  트와이스      7              1.0  ...          1.0            1.0            1.0
+
+[7 rows x 7 columns]
+
+'''
+"""
+#텐서플로우 사용 선형? 비선형?
+
+import pandas as pd
+import tensorflow as tf
+import numpy as np
+from sklearn.model_selection import train_test_split
+
+tf.set_random_seed(777)
+
+#데이터 불러오기
+mydata = pd.read_csv('../collected_data/가수.csv', encoding = 'EUC-KR')
+
+x = mydata['가수']
+y = mydata['rank']
+x = x.values.reshape(x.size, 1)
+y = y.values.reshape(y.size, 1)
+
+#학습 7 테스트 3비율로 나누기
+#train_test_split 를 사용
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3,random_state=0)
+print(x_train.shape)
+print(x_test.shape)
+print(y_train.shape)
+print(y_test.shape)
+
+
+#모델 학습
+
+X = tf.placeholder(tf.float32, shape=[None, 1])
+Y = tf.placeholder(tf.float32, shape=[None, 1])
+
+W1 = tf.get_variable('W1', shape=[1, 2], initializer=tf.contrib.layers.xavier_initializer())
+b1 = tf.Variable(tf.random_normal(shape=[2]))
+net = tf.matmul(X, W1) + b1
+net = tf.nn.relu(net)
+
+W2 = tf.get_variable('W2', shape=[2, 2], initializer=tf.contrib.layers.xavier_initializer())
+b2 = tf.Variable(tf.random_normal(shape=[2]))
+net = tf.matmul(net, W2) + b2
+net = tf.nn.relu(net)
+
+W3 = tf.get_variable('W3', shape=[2, 1], initializer=tf.contrib.layers.xavier_initializer())
+b3 = tf.Variable(tf.random_normal(shape=[1]))
+prediction = tf.matmul(net, W3) + b3
+
+loss = tf.reduce_mean(tf.square(prediction - Y))
+optimizer = tf.train.AdamOptimizer(learning_rate=0.01)
+train_op = optimizer.minimize(loss)
+
+sess = tf.Session()
+sess.run(tf.global_variables_initializer())
+sess.run(tf.local_variables_initializer())
+
+epochs = 30
+for epoch_index in range(epochs):
+    sess.run(train_op, feed_dict={X: x_train, Y: y_train})
+
+#모델 검증
+
+    loss_value_train = sess.run(loss, feed_dict={X: x_train, Y: y_train})
+    loss_value_test = sess.run(loss, feed_dict={X: x_test, Y: y_test})     
+    print('epoch: {}/{}, train loss: {:.4f}, test loss: {:.4f}'.format(
+        epoch_index+1, epochs, loss_value_train, loss_value_test))
+
+#모델 예측
+
+y_predict = sess.run(prediction, feed_dict={X: [[544]]})
+print(y_predict) #[[8.182088]]
+print(y_predict.flatten()) #[8.182088]
+print(y_predict.flatten()[0]) #8.182088
