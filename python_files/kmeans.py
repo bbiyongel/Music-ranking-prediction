ed8d47afd1890709194514a8b2075a6049fce574
jjony553 <55618626+jjony553@users.noreply.github.com>
2019-11-09 16:58:47 +0900

    k-mean clustering
    20번 이상 차트 진입한 가수데이터를 활용함

--- python_files/kmeans.py
+++ python_files/kmeans.py
@@ -5,12 +5,12 @@ import pandas as pd
 import numpy as np
 
 #get data from csv
-Data = pd.read_csv('../collected_data/kmeandata.csv', encoding = 'EUC-KR')
-x = Data['rank']
-y = Data['가수']
+Data = pd.read_csv('ksinger20data.csv', encoding = 'EUC-KR')
+x = Data['가수']
+y = Data['rank']
 
 #Extract freature
-feature = Data[['rank','가수']]
+feature = Data[['가수','rank']]
 feature.head()
 
 
@@ -23,13 +23,12 @@ predict.columns=['predict']
 #concatenate predict
 r = pd.concat([feature, predict], axis=1)
 print(r)
-
-centers = pd.DataFrame(kmeans.cluster_centers_,columns=['rank','가수'])
-center_x = centers['rank']
-center_y = centers['가수']
+centers = pd.DataFrame(kmeans.cluster_centers_,columns=['가수','rank'])
+center_x = centers['가수']
+center_y = centers['rank']
 
 #Visualize result
-plt.scatter(r['rank'],r['가수'],c=r['predict'],alpha=0.5)
+plt.scatter(r['가수'],r['rank'],c=r['predict'],alpha=0.5)
 plt.scatter(center_x,center_y,s=50,marker='D',c='r')
 
 #scatter plot
